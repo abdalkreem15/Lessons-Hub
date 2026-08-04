31c7dc0c8db4102d51d8644a4f08c471fb83500e
Abdalkareem <abdalkareemalkafoury@gmail.com>
2026-08-04 11:31:03 +0300

    added nothing just changed the avatar of the teachers to be more realistic and professional(untill i add actual teachers (as this is a test application))

--- LessonsHub/src/lib/data.ts
+++ LessonsHub/src/lib/data.ts
@@ -19,7 +19,7 @@ export const teachers: Teacher[] = [
     id: '1',
     name: 'Dr. Ahmed Mohamed',
     subjectIds: ['math'],
-    avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=150',
+    avatar: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?w=150',
     bio: 'Professor of Advanced Mathematics with 10+ years helping students excel.',
     price: 250,
     availableSlots: ['Saturday 4:00 PM', 'Monday 6:00 PM', 'Wednesday 8:00 PM']
@@ -28,7 +28,7 @@ export const teachers: Teacher[] = [
     id: '2',
     name: 'Eng. Sarah Hassan',
     subjectIds: ['physics', 'math'],
-    avatar: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?w=150',
+    avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=150',
     bio: 'Software engineer and physics tutor making mechanics simple and intuitive.',
     price: 200,
     availableSlots: ['Sunday 2:00 PM', 'Tuesday 5:00 PM', 'Thursday 7:00 PM']
